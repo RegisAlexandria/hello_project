520320b787c7ec9bbb092d0338583e8481d774ea
regis_alexandria <regisbarrosdealexandria@hotmail.com>
2024-05-02 10:38:31 -0300

    alteracao app3

--- myExpressApp/app.js
+++ myExpressApp/app.js
@@ -35,7 +35,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('errorsss');
+  res.render('errorssss');
 });
 
 module.exports = app;
